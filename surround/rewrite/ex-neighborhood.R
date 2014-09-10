febae3fab4ce57ee1e70b8184ca3b0c8dfab6a00
Noah Peart <noah.v.peart@gmail.com>
2014-09-10 16:18:27 -0400

    Batch update

--- surround/rewrite/ex-neighborhood.R
+++ surround/rewrite/ex-neighborhood.R
@@ -22,7 +22,7 @@ ex_neighborhood <- function(radius=1.5, numQuads=8, nbrs=NULL,
     if (is.null(nbrs) && rand) {
         if (is.null(numNebs))
             numNebs <- sample(1:12,1)
-        nbrs <- make_nbrs(numNebs)
+        nbrs <- make_nbrs(numNebs,radius)
         if (nrow(nbrs) < 1) {
             nbrs <- make_nbrs(numNebs)
         }
@@ -42,8 +42,36 @@ ex_neighborhood <- function(radius=1.5, numQuads=8, nbrs=NULL,
                                    breaks = seq(0, 2*pi, length.out = numQuads+1),
                                    right = FALSE))
 
-    ## Draw quadrant lines
+    ## Calculate components
     rad <- 2*pi / numQuads
+    filled <- unique(pcoords$quad)
+    if (length(filled) == numQuads) {
+        pcoords$rad <- 2*pi # full surround
+    } else if (length(filled) == 1) {
+        pcoords$rad <- rad # single neighbor
+    } else {
+        strt <- which(is.na(match(1:numQuads, filled)))[[1]] # start from 1st open sector
+        ord <- filled
+        ord[ord < strt] <- ord[ord < strt] + numQuads # reorder from new start sector
+        ord <- sort(ord)
+        runs <- rle(diff(ord)) # find runs of filled sectors
+        if (!any(runs$values==1)) {
+            pcoords$rad <- rad # no connected comps
+        } else {
+            rads <- rep(rad, length(runs$values)) # initial radians of each sector
+            rads[which(runs$values==1)] <- runs$lengths[runs$values==1] * rad + rad # radians of continuous sectors
+            runs$lengths[runs$values==1] <- runs$lengths[runs$values==1]+1 # increm. connected comps
+            decrem <- which(runs$values == 1) - 1 # fix diff offset of 1
+            decrem <- decrem[decrem > 1]
+            if (length(decrem) > 0)
+                runs$lengths[decrem] <- runs$lengths[decrem] - 1
+            out <- rep(rads, runs$lengths)
+            outInds <- (ord - 1) %% numQuads + 1
+            pcoords$rad <- out[match(pcoords$quad, outInds)]
+        }
+    }
+
+    ## Draw quadrant lines
     ps <- data.frame(pol2cart(r = radius * 2,
                               theta = seq(0, 2*pi, by = rad)))
     for (i in 1:nrow(ps))
@@ -52,7 +80,6 @@ ex_neighborhood <- function(radius=1.5, numQuads=8, nbrs=NULL,
               col = "blue", lty = 2)
 
     ## Fill in occupied quadrats
-    ## **** WORKING ON THE POLYGONS... ****
     for (i in 1:numQuads) {
         if (i %in% pcoords$quad) { # quadrant is occupied, fill it
             angle1 <- (i - 1) * rad
@@ -63,6 +90,17 @@ ex_neighborhood <- function(radius=1.5, numQuads=8, nbrs=NULL,
                     col = "steelblue", density = 10,
                     lty = 2, lwd = 2, angle = atan((2*angle1+rad)/2) * 180/pi + 90)
         }
-
     }
+
+    ## Write in component radians
+    print(pcoords)
+    xy <- data.frame(pol2cart(pcoords$r, pcoords$theta))
+    xy$rad <- round(pcoords$rad, 4)
+    xy <- unique(xy)
+    text(xy, labels = xy$rad)
+
+    return ( list(nbrs=nbrs, pcoords=pcoords) )
 }
+
+aa <- 1:8
+tst <- c(1,2,3,5,6)
