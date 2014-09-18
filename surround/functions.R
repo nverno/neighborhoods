b7b058b2427f12bc6f977e81270fa9ee1d65727e
Noah Peart <noah.v.peart@gmail.com>
2014-09-18 18:18:39 -0400

    Batch Update

--- surround/functions.R
+++ surround/functions.R
@@ -3,7 +3,7 @@
 ##  Functions for Neighborhood Surround Index (NSI) and testing NSI
 ##
 ###############################################################################
-
+##
 ## Euclidean distance
 euc <- function(p1, p2)
     sqrt((p1[1]-p2[1])^2 + (p1[2]-p2[2])^2)
