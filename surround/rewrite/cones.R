b03bd054e9eee7d077a10b243da26413591bc963
Noah Peart <noah.v.peart@gmail.com>
2014-11-24 23:31:44 -0500

    Batch update

--- surround/rewrite/cones.R
+++ surround/rewrite/cones.R
@@ -3,7 +3,7 @@
 ## Description: Conical neighbors
 ## Author: Noah Peart
 ## Created: Mon Nov 24 18:54:32 2014 (-0500)
-## Last-Updated: Mon Nov 24 22:49:30 2014 (-0500)
+## Last-Updated: Mon Nov 24 23:31:22 2014 (-0500)
 ##           By: Noah Peart
 ######################################################################
 source("~/work/neighborhoods/surround/rewrite/spheres.R")
@@ -56,20 +56,80 @@ ellipse_short_axis <- function(targ, nbr) {
     p_targ <- c(0, 0, targ[["ht"]])
     tn <- p_center - p_targ  # vector from targ -> center neighbor base
     te <- p_closest - p_targ  # vector from targ -> edge of neighbor base closest to targ
-    theta <- 
+    theta <- acos(crossprod(tn, te)/(sqrt(sum(tn**2))*sqrt(sum(te**2))))  # short axis in radians
+    return ( theta )
 }
 
-## Find opening angle, theta, of cone formed from target to spherical neighbor
-## and neighbors crown radius, r
-## NOTE: crown radius of neighbor is the minimum of its actual crown radius
-## and the distance to target (to avoid condition where target's crown is
-## enveloped by neighbor's crown)
-cone_theta <- function(nbr) {
-    r <- min(c(sqrt(nbr[["crarea"]]/pi), nbr[["dist"]]))  # if crown of nbr obscures target, set to dist
-    d <- nbr$dist
-    theta1 <- asin(r/d)
-    theta <- theta1*2
-    out <- c(r, theta)
-    names(out) <- c("r", "theta")
-    return ( out )
+################################################################################
+##
+##      Create pixel matrix and image, treating all neighbors as cones
+##
+################################################################################
+## Determine vertical radian from positive x-axis (0 radians)
+## NOTE: relative to origin (0, 0, 0)
+v_rad <- function(x, z) {
+    if (x == 0 && z == 0)
+        return ( 0 )
+    theta <- atan(z/x)
+    theta[x < 0] <- theta[x < 0] + pi
+    theta[x >= 0 & z < 0] <- theta[x >= 0 & z < 0] + 2*pi
+    return ( theta )
+}
+
+## Convert neighborhood into pixel matrix
+## Rows are horizontal radians, cols are vertical
+## positive x-axis is 0 radians in both dims
+pixel_matrix <- function(targ, nbrs, size=10, precise=TRUE) {
+    mat_ind <- seq(0, 2*pi, length.out=size)
+    mat <- matrix(0, nrow=size, ncol=size)
+    inc <- 2*pi/size
+        
+    for (i in 1:nrow(nbrs)) {
+        n <- nbrs[i,]
+        b <- ellipse_long_axis(n)
+        a <- ellipse_short_axis(targ, n)
+        h <- height_angle(nbr)
+        pos <- c(n[["x"]], n[["y"]], n[["ht"]] - n[["crdepth"]] + n[["z"]])
+        names(pos) <- c("x", "y", "z")
+
+        ## horizontal/vertical radians
+        pol <- cart2pol(n[["x"]], n[["y"]])
+        hrad <- c(-1,1)*b/2 + pol[["theta"]]
+        w_ht <- ifelse(h < 2*a, 2*a, a+h)
+
+        ## STOPPED HERE!!!
+        vrad_ellipse <- c(-1,1)*a + v_rad(pos[["x"]], pos[["z"]] - targ[["ht"]])
+        vrad_both <- c(-1,1)*w_ht/2 + v_rad(pos[["x"]], pos[["z"]] - targ[["ht"]])
+
+        ## Fill matrix (square version)
+        rows <- which(mat_ind >= min(hrad) - inc & mat_ind <= max(hrad) + inc)
+        cols <- which(mat_ind >= min(vrad) - inc & mat_ind <= max(vrad) + inc)
+        
+        if (!precise) {
+            mat[rows, cols] <- 1
+        } else {
+            ## More precise version, find pixels within inscribed circle
+            ## Check square for pixels <= theta/2 euclidean distance
+            dmat <- euc(t(as.matrix(expand.grid(rows*2*pi/size, cols*2*pi/size))),
+                        c(mean(hrad), mean(vrad)))
+            dmat <- matrix(ifelse(dmat <= theta/2, 1, 0), nrow=length(rows), ncol=length(cols))
+            mat[rows, cols] <- mat[rows, cols] + dmat
+            ## sum(dmat <= theta/2)/length(dmat)  # should approach pi/4 if circle exactly inscribed
+        }
+    }
+    return ( mat )
+}
+
+## Image of matrix
+image_hood <- function(targ, nbrs, size=100, precise = TRUE) {
+    pmat <- pixel_matrix(targ, nbrs, size=size, precise=precise)
+    mat_ind <- seq(0, 2*pi, length.out = size)
+    image(mat_ind, mat_ind, pmat, xlab = "Horizontal Radians",
+          ylab = "Vertical Radians",
+          main = "Neighborhood Image Relative to Target Crown\n(Positive x-axis, Radians=0)")
+    abline(h=pi, v=pi)
+    abline(v = seq(0, 2*pi, by = pi/2), lty = 2)
+    ## text(4, 3, "Above Target")
+    text(seq(0.5, 2*pi-0.5, length.out = 4), y=pi, c("South", "East", "North", "West"), cex=2)
+    text(c(pi, pi), y = c(0.5, 2*pi-0.5), c("Above Target", "Below Target"), cex = 2)
 }
