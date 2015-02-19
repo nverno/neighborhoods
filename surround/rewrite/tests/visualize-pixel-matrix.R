0c691253a7b19248dbb2fcc2715377d7b9e28e2c
Noah Peart <noah.v.peart@gmail.com>
2015-02-18 23:37:13 -0500

    Batch Update

--- surround/rewrite/tests/visualize-pixel-matrix.R
+++ surround/rewrite/tests/visualize-pixel-matrix.R
@@ -3,7 +3,7 @@
 ## Description: Visual tests for pixel matrix
 ## Author: Noah Peart
 ## Created: Wed Feb 18 13:53:11 2015 (-0500)
-## Last-Updated: Wed Feb 18 16:48:09 2015 (-0500)
+## Last-Updated: Wed Feb 18 23:36:51 2015 (-0500)
 ##           By: Noah Peart
 ######################################################################
 source("~/work/nbrs3d/vis.R")  # 3d visualization stuff (p3d, rgl shapes)
@@ -12,14 +12,18 @@ source("~/work/neighborhoods/surround/rewrite/pixel_matrix.R")
 draw_hood_full(nbrs = nbrs)  # 2D representation
 p3d(nbrs)  # 3D
 abclines3d(0, a = diag(4), col = "slategray")
+add_tree(targ, wire=TRUE)
 
-## Add target crown to 3D plot
-add_target <- function(targ, wire = FALSE, crwn_col = "darkred") {
-    require(rgl)
-    if (all(sapply(c(targ$x, targ$y, targ$z), is.null)))
-        targ[, c("x", "y", "z")] <- c(0,0,0)  # target centered in plot
-    tt <-  targ[complete.cases(targ[,c("x","y","z","dbh","ht","crdepth","crarea")]), ]
-    p3d(tt, crwn_wire = wire, crwn_col = crwn_col)
+
+
+m <- diag(4)
+m[1,] <- c(0, 1, 1, 0)
+abclines3d(0, a = m, col = "darkred", lwd = 3)
+
+## Draw 3D tangent lines
+add_tangents <- function(targ, nbr, ...) {
+    r <- min(c(sqrt(nbr[["crarea"]]/pi), nbr[["dist"]]))  # if crown of nbr obscures target, set to dist
+    d <- nbr$dist
+    theta1 <- asin(r/d)
+    theta <- theta1*2
 }
-
-add_target(targ)
